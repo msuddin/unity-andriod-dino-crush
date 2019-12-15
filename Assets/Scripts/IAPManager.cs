c2b589d3b2b5fa50d3a0a6492c33f0f1cd5c0361
msuddin <mo@thetestroom.com>
2019-12-15 21:23:53 +0000

    Added skeleton in-app purchase

---
+++ Assets/Scripts/IAPManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class IAPManager : MonoBehaviour, IStoreListener
+{
+    private static IStoreController m_StoreController;          
+    private static IExtensionProvider m_StoreExtensionProvider; 
+
+    public static string BUY_LIFE = "<insert_here_id_of_in_app_purchase>";
+
+    private void Start()
+    {
+        if (m_StoreController == null)
+        {
+            InitializePurchasing();
+        }
+    }
+
+    public void InitializePurchasing()
+    {
+        if (IsInitialized())
+        {
+            return;
+        }
+
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+
+        builder.AddProduct(BUY_LIFE, ProductType.Consumable);
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+
+    private bool IsInitialized()
+    {
+        return m_StoreController != null && m_StoreExtensionProvider != null;
+    }
+
+
+    public void Buy_life()
+    {
+        BuyProductID(BUY_LIFE);
+    }
+
+    void BuyProductID(string productId)
+    {
+        if (IsInitialized())
+        {
+            Product product = m_StoreController.products.WithID(productId);
+
+            if (product != null && product.availableToPurchase)
+            {
+                Debug.Log(string.Format("Purchasing product asychronously: '{0}'", product.definition.id));
+                m_StoreController.InitiatePurchase(product);
+            }
+            else
+            { 
+                Debug.Log("BuyProductID: FAIL. Not purchasing product, either is not found or is not available for purchase");
+            }
+        }
+        else
+        {
+            Debug.Log("BuyProductID FAIL. Not initialized.");
+        }
+    }
+
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        Debug.Log("OnInitialized: PASS");
+
+        m_StoreController = controller;
+        m_StoreExtensionProvider = extensions;
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        Debug.Log("OnInitializeFailed InitializationFailureReason:" + error);
+    }
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
+    {
+        if (String.Equals(args.purchasedProduct.definition.id, BUY_LIFE, StringComparison.Ordinal))
+        {
+            Debug.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+        }
+        else
+        {
+            Debug.Log(string.Format("ProcessPurchase: FAIL. Unrecognized product: '{0}'", args.purchasedProduct.definition.id));
+        }
+
+        return PurchaseProcessingResult.Complete;
+    }
+
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        Debug.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: {1}", product.definition.storeSpecificId, failureReason));
+    }
+}
--- README.md
+++ README.md
@@ -25,14 +25,14 @@ For this project, I have decided to cover the following areas:
     - [x] Audio effects
     - [x] Particles
   - [x] Game background audio
-  - [ ] Screens / Scenes
+  - [x] Screens / Scenes
     - [x] Main menu
-    - [ ] Store screen (integration for inApp purchases)
+    - [x] Store screen (integration for inApp purchases)
     - [x] Gameplay screen
 - [x] Creating game sprites
 - [x] Writing automated
   - [x] Unit tests
-- [ ] Integration with InApp Store purchase
+- [x] Integration with InApp Store purchase
 - [x] Integration for Mobile Ads
 
 Once all the above areas have been marked off, this should be enough to get started on a first possible Play Store Android project.
@@ -67,4 +67,15 @@ https://opengameart.org/content/5-chiptunes-action
  - Double power particles appear behind the game background as well as the bullets fired (disabled background to allow bullet and particle visibility)
  - Falling animation is a little unstable
  - Sometimes a pokeball item may leave behind an invisible box that is not removed when the player collides (and sometims not) with a given pokeball
+ - Have removed all instanced of Text Mesh Pro as the game started to fail builds due to Unity engine incompatibility
+ - All text assets such as ball count, player and boss health have been removed
 
+
+## Some Learning
+ - During the developments of a game, do not update your version of Unity - not until the game is finished
+ - Do small commits with tests for each commit - do not commit two different things in one commit
+ - Regularly ship the product on devices for people to look at
+
+
+## Conclusiong
+As part of this project, I find myself having learnt some of the basics of Android Game Development. I have learnt about game assets, coding in C#, monitization and just general fun. I think for my next game project, I will find some actual end customers and develop a game to their needs :)
